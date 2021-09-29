12d30bd2727146aaa2aee5d069c2760ad4b4f978
JuanPedroSanchezAlvarez <juanpedrosanchez1989@gmail.com>
2021-09-29 10:33:33 +0200

    Initial commit.

---
+++ thyme-wizards/src/main/java/com/thymewizards/ThymeWizardsApplication.java
@@ -0,0 +1,13 @@
+package com.thymewizards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeWizardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeWizardsApplication.class, args);
+	}
+
+}
---
+++ thyme-wizards/src/test/java/com/thymewizards/ThymeWizardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymewizards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeWizardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
