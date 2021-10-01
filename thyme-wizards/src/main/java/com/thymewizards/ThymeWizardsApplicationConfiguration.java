f6e8eac85067cab596fc48d6173e22419a136b7a
JuanPedroSanchezAlvarez <juanpedrosanchez1989@gmail.com>
2021-10-01 12:39:38 +0200

    Refactor with Thymeleaf Fragments.

---
+++ thyme-wizards/src/main/java/com/thymewizards/ThymeWizardsApplicationConfiguration.java
@@ -0,0 +1,21 @@
+package com.thymewizards;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class ThymeWizardsApplicationConfiguration {
+
+	// We instruct Thymeleaf to search for fragments in the svg directory using the .svg suffix (as opposed to the default .html suffix).
+	@Bean
+	public ITemplateResolver svgTemplateResolver() {
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setPrefix("classpath:/templates/svg/");
+		resolver.setSuffix(".svg");
+		resolver.setTemplateMode("XML");
+		return resolver;
+	}
+
+}
